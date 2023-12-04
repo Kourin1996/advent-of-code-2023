257b7b97b9a814a1c09cd9abc30d7446a976ff31
kourin <kourin.code@gmail.com>
2023-12-04 15:37:55 +0700

    Day3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     // day1::part1::solve();
     // day1::part2::solve();
     // day2::part1::solve();
-    day2::part2::solve();
+    // day2::part2::solve();
+    // day3::part1::solve();
+    day3::part2::solve();
 }
