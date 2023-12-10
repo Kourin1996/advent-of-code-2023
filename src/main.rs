8357db2710eba91175ba6e8408ad3cb30cb39b82
kourin <kourin.code@gmail.com>
2023-12-10 08:24:10 +0700

    Day4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     // day1::part1::solve();
@@ -8,5 +9,7 @@ fn main() {
     // day2::part1::solve();
     // day2::part2::solve();
     // day3::part1::solve();
-    day3::part2::solve();
+    // day3::part2::solve();
+    // day4::part1::solve();
+    day4::part2::solve();
 }
